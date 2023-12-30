28686aaf424037e75bf5306e25fa4049abb60946
TheThirdSwan <aryaswan17@gmail.com>
2023-12-31 07:59:23 +0800

    added item sprites

--- script/Knight.gd
+++ script/Knight.gd
@@ -1,5 +1,13 @@
 extends KinematicBody2D
 
+#variabel node
+onready var character = $"."
+onready var textlist = $CanvasLayer/itemlist
+onready var textnotif = $CanvasLayer/itemlist/itemnotification
+onready var timer = $CanvasLayer/itemlist/itemnotification/Timer
+onready var sword = $sword
+onready var shield = $shield
+
 #variabel
 var velocity = Vector2()
 const speed = 60
@@ -7,6 +15,7 @@ const maxspeed = 600
 const jump = -150
 const gravity = 3
 const up = Vector2(0, -1)
+var itemlist = []
 
 func _process(delta):
 	# aplikasi gravitasi
@@ -16,6 +25,10 @@ func _process(delta):
 	if Input.is_action_pressed("kiri"):
 		$AnimatedSprite.play("move")
 		$AnimatedSprite.flip_h = true
+		$sword.flip_h = true
+		$sword.offset.x = -25
+		$shield.flip_h = true
+		$shield.offset.x = -12
 		velocity.x -= min(speed * delta, maxspeed)
 	#jika berhenti kiri, idle kiri
 	elif Input.is_action_just_released("kiri"):
@@ -25,6 +38,10 @@ func _process(delta):
 	elif Input.is_action_pressed("kanan"):
 		$AnimatedSprite.play("move")
 		$AnimatedSprite.flip_h = false
+		$sword.flip_h = false
+		$sword.offset.x = 0
+		$shield.flip_h = false
+		$shield.offset.x = 0
 		velocity.x += min(speed * delta, maxspeed)
 	#jika berhenti kanan, idle kanan
 	elif Input.is_action_just_released("kanan"):
@@ -40,3 +57,53 @@ func _process(delta):
 
 	#untuk menggerakkan karakter
 	velocity = move_and_slide(velocity, up)
+	
+	for items in itemlist:
+		match items:
+			"Stone Sword":
+				sword.visible = true
+			"Shield of the Tree":
+				shield.visible = true
+		pass
+
+func _on_chestunder_pressed():
+	itemlist.append("Stone Sword")
+	displayitemnotif("Stone Sword")
+	displayitemlist()
+	pass
+
+
+func _on_chesttree_pressed():
+	itemlist.append("Shield of the Tree")
+	displayitemnotif("Shield of the Tree")
+	displayitemlist()
+	pass
+
+#menampilkan notifikasi barang
+func displayitemnotif(item):
+	textnotif.visible = true
+	textnotif.text = item + " didapatkan."
+	timer.start()
+	pass
+
+#menampilkan daftar barang
+func displayitemlist():
+	textlist.text = "Barang : \n"
+	for items in itemlist:
+		textlist.text += items + "\n"
+	pass
+
+#timer, jika waktu habis, menyembunyikan notifikasi
+func _on_Timer_timeout():
+	textnotif.visible = false
+	pass
+
+
+func _on_start_pressed():
+	character.set_process_input(true)
+	pass
+
+
+func _on_knight_ready():
+	character.set_process_input(false)
+	pass # Replace with function body.
--- script/Main.gd
+++ script/Main.gd
@@ -2,31 +2,21 @@ extends Control
 
 #inisialisasi variabel node
 onready var tilemap = $TileMap
-onready var textlist = $CanvasLayer/itemlist
-onready var textnotif = $CanvasLayer/itemlist/itemnotification
-onready var timer = $CanvasLayer/itemlist/itemnotification/Timer
 onready var buttonunder = $chest1/chestunder
 onready var buttontree = $chest2/chesttree
-onready var gameover = $CanvasLayer/gameover
-onready var gameoverbutton = $CanvasLayer/gameover/Panel/gameoverbutton
+onready var gameover = $Knight/CanvasLayer/gameover
+onready var gameoverbutton = $Knight/CanvasLayer/gameover/Panel/gameoverbutton
 
 var openchest = 31
-var itemlist = []
 
 #saat tombol buka peti bawah tanah ditekan, tileset peti diubah, menambahkan item ke array itemlist
 func _on_chestunder_pressed():
 	tilemap.set_cell(57, 34, openchest)
-	itemlist.append("Stone Sword")
-	displayitemnotif("Stone Sword")
-	displayitemlist()
 	pass
 
 #saat tombol buka peti atas pohon ditekan, tileset peti diubah, menambahkan item ke array itemlist
 func _on_chesttree_pressed():
 	tilemap.set_cell(30, 20, openchest)
-	itemlist.append("Shield of the Tree")
-	displayitemnotif("Shield of the Tree")
-	displayitemlist()
 	pass
 
 #untuk mengecek kedua peti
@@ -40,40 +30,13 @@ func _process(delta):
 		
 	pass
 
-#menampilkan notifikasi barang
-func displayitemnotif(item):
-	textnotif.visible = true
-	textnotif.text = item + " didapatkan."
-	timer.start()
-	pass
-
-#menampilkan daftar barang
-func displayitemlist():
-	textlist.text = "Barang : \n"
-	for items in itemlist:
-		textlist.text += items + "\n"
-	pass
-
-#timer, jika waktu habis, menyembunyikan notif barang
-func _on_Timer_timeout():
-	textnotif.visible = false
-	pass # Replace with function body.
-
-
-
 func _on_fallarea_body_entered(body):
 	gameover.visible = true
 	gameoverbutton.disabled = false
 	pass
-
 
 func _on_gameoverbutton_pressed():
 	gameover.visible = false
 	gameoverbutton.disabled = true
 	get_tree().reload_current_scene()
 	pass
-
-
-func _on_start_pressed():
-
-	pass # Replace with function body.
